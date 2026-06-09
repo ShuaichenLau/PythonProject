0d3404ae270c103f4714dda1009fc8c2fd1a511f
ShuaichenLau <68754807@qq.com>
2026-06-09 21:12:02 +0800

    代码练习

--- src/demo7.py
+++ src/demo7.py
@@ -9,4 +9,17 @@ print(list(lst))
 for e in zip(items, prices):
     print(e)
 
+# 空列表
+lst = []
+lst1 = list()
+# 空字典
+d = {}
+d2 = dict()
+# 空元祖
+t4 = ()
+t5 = tuple()
 
+# 在python中 列表/字典/元祖 区别是什么?
+
+print(type(lst),type(d),type(t4))
+print(type(lst1),type(d2),type(t5))
