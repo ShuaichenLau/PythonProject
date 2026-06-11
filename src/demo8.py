23c3eaf06fa85a016d57d3ab3244c83c106f2645
ShuaichenLau <68754807@qq.com>
2026-06-11 19:19:10 +0800

    代码练习-集合

--- src/demo8.py
+++ src/demo8.py
@@ -1,4 +1,39 @@
 t=(10,[20,30],9)
 print(type(t),t)
 
-print(type(t),t)
+# 元祖的便利
+
+t=('Python','Word',98)
+print(t[0])
+print(t[1])
+print(t[2])
+
+for item in t:
+    print(item)
+print("====================")
+
+
+
+list={10,20,30}
+print(10 in list)
+print(10 not in list)
+
+list.add(40)
+print(list)
+
+list.update({50,60,70,90})
+print(list)
+
+list.remove(90)
+print(list)
+
+list.discard(90)
+print(list)
+
+list.pop()
+print(list)
+
+list.clear()
+print(list)
+
+
