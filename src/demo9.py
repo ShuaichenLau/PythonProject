a64b8970047bc6757473cd9c368e461872a8352b
ShuaichenLau <68754807@qq.com>
2026-06-13 14:50:10 +0800

    代码练习-mysql

--- src/demo9.py
+++ src/demo9.py
@@ -14,3 +14,21 @@ print(list_a.isdisjoint(list_b))
 print(list_a.union(list_b))
 # 差集
 print(list_c.difference(list_a))
+
+
+print(id(list_a))
+
+hello = ' hello,hello'
+
+print("count {}".format(hello.count('hello')))
+print("find {}".format(hello.find('h')))
+print("rfind {}".format(hello.rfind('h')))
+print("index {}".format(hello.index('h')))
+print("rindex {}".format(hello.rindex('h')))
+
+print("not find {}".format(hello.find('A')))
+
+# print("大小写  index {}".format(hello.index('H')))
+
+
+
