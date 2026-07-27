a3dc13a7925cb8f148121889493feb56b88a6a79
ShuaichenLau <68754807@qq.com>
2026-07-27 21:17:51 +0800

    动态绑定对象属性方法

---
+++ src/demo15.py
@@ -0,0 +1,15 @@
+class Car:
+    def __init__(self, brand):
+        self.brand = brand
+
+    def start(self):
+        print('{} 汽车已经启动'.format(self.brand))
+
+    def stop(self):
+        print('{} 汽车已经停止'.format(self.brand))
+
+
+bmw=Car('BMW_X3')
+bmw.start()
+
+print(dir(bmw))
