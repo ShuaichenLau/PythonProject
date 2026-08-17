3f1b8f2f0fee28cd688ddd5968e1758d6959e896
liusc <68754807@qq.com>
2026-08-17 14:25:43 +0800

    feat(io): 添加文件操作和系统交互演示功能
    - 实现文件读写操作示例，包含不同模式下的文件处理
    - 集成上下文管理器进行文件资源管理
    - 添加系统目录操作和文件属性判断功能
    - 创建文件夹管理和路径获取相关代码示例

---
+++ src/demo15.py
@@ -0,0 +1,43 @@
+'''
+模式含义如果文件存在如果文件不存在常用场景
+'r'只读 (Read)从头开始读取报错 (FileNotFoundError)默认模式，读取配置文件或文本
+'w'只写 (Write)直接清空旧内容并覆盖自动创建新文件重新生成日志、保存新数据
+'a'追加 (Append)保留原内容，在末尾写入自动创建新文件记录日志（Log）、追加日志行
+'x'排他创建 (eXclusive)报错 (FileExistsError)自动创建新文件防止误覆盖已有文件（安全写入）
+'''
+
+# 声明list
+lines=[]
+
+#  上下文管理器
+with open('data.txt','r',encoding='UTF-8') as file:
+    for line in file:
+        print(line)
+        lines.append(line)
+
+
+print("=========================")
+print("打印输出",lines)
+
+# with open('output.txt', 'w', encoding='UTF-8') as out_file:
+
+with open('output.txt','w',encoding='UTF-8') as out_file:
+    out_file.writelines(lines)
+print("=========================")
+
+
+with (open('output1.txt','w',encoding='UTF-8') as out_file1):
+    for line in lines:
+        out_file1.write(line)
+    out_file1.write('\n')
+    out_file1.write('HelloWorld+++\n')
+    out_file1.writelines('HelloWorld===')
+print("=========================")
+
+
+
+
+
+
+
+
